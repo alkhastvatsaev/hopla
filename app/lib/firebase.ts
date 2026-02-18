f899840aa5dccd04b2c4e5bd5a5a7aea91a81225
alkhastvatsaev <alkhastvatsaev@icloud.com>
2026-02-18 07:45:30 +0100

    chore: use environment variables for Firebase configuration

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAJ7jfeSwzA_OnZJxvoR0iSDOiCaIStSaw",
-  authDomain: "hopla-7bfe3.firebaseapp.com",
-  projectId: "hopla-7bfe3",
-  storageBucket: "hopla-7bfe3.firebasestorage.app",
-  messagingSenderId: "977059782327",
-  appId: "1:977059782327:web:275d74ed2fdd213e92f732",
-  measurementId: "G-C51QM3EJW5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
